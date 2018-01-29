fcba3a4a05a4fbdfcb36fda6ba4acdb8656f207f
Cody <codyhusek@codys-imac-pro.nycap.rr.com>
2018-01-29 15:08:02 -0500

    Added UserDefaults test

--- HuPOS/ViewController.swift
+++ HuPOS/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        UserDefaults.standard.set("test", forKey:"Test")
+    
         // Do any additional setup after loading the view, typically from a nib.
     }
 
