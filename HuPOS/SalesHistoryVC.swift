8426301e315f89f310eb864b80e5687f11eb03d0
Cody <codyhusek@codys-imac-pro.local>
2018-05-03 20:49:01 -0400

    c

---
+++ HuPOS/SalesHistoryVC.swift
@@ -0,0 +1,46 @@
+//
+//  SalesHistoryVC.swift
+//  HuPOS
+//
+//  Created by Cody Husek on 5/3/18.
+//  Copyright © 2018 HuSoft Solutions. All rights reserved.
+//
+
+import UIKit
+
+class SalesHistoryVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var sales:[Sale] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.sales.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
